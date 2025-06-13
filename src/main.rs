6b53c79db5919357e918a1d4a67ea9d7868f94d4
roket64 <1p41i4y1@gmail.com>
2025-06-13 16:49:17 +0900

    added example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use novelapi_lib::{client::NAIClient, preset::ImagePresetBuilder};
+
+pub async fn generate_image_v4_example() {
+    dotenv::dotenv().ok();
+
+    let username = dotenv::var("USERNAME").unwrap();
+    let password = dotenv::var("PASSWORD").unwrap();
+
+    let client = NAIClient::new(&username, &password).await;
+
+    let preset = ImagePresetBuilder::new()
+        .prompt(String::from("1girl"))
+        .build();
+
+    let _ = client.generate_image(preset).await;
+}
+
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+
+    generate_image_v4_example().await;
 }
